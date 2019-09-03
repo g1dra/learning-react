7ba33d19a1f4532050298bc4b67543cfd200d072
Darko <darko.vucetic7@gmail.com>
2019-09-03 15:53:04 +0200

    8.13 done

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
-import classes from './Layout.module.css'
+import classes from './Layout.module.css';
 
 const layout = ( props  ) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
             <main className={classes.Content}>
-            {props.children}
+                {props.children}
             </main>
     </Aux>
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+
 import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+    // constructor(props){
+    //     super(props);
+    //     this.state = {...}
+    // }
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    };
+
     render () {
         return (
             <Aux>
-                <div>Burger graphical representation</div>
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients }/>
+                <div>Build Controlss</div>
             </Aux>
         );
     }
